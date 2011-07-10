fb69b2da122b467f9614ed08826deb8701671d00
Andrei Antoukh <niwi@niwi.be>
2011-07-10 17:39:34 +0200

    Add database autentication.

--- settings.js
+++ settings.js
@@ -4,9 +4,10 @@ module.exports = {
     dbname: 'test2',
     dburl: 'http://localhost',
     dbport: 5984,
-    dbsettings: {cache: true, raw: false},
+    dbsettings: {cache: true, raw: false,
+        auth: { username: 'admin', password: '123123' }},
     get_database: function() {
-        var connection = new(cradle.Connection)(this.dburl, this.dbport);
+        var connection = new(cradle.Connection)(this.dburl, this.dbport, this.dbsettings);
         var database = connection.database(this.dbname);
         database.exists(function(err, value){
             if(!value && !err) { database.create(); }
