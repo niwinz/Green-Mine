14b93b6808be562aa7ad545452599009c4e3c4d7
Juanfran <juanfran.ag@gmail.com>
2011-08-15 12:22:54 +0200

    adding filters to the dashboard

--- maqueta/js/general.js
+++ maqueta/js/general.js
@@ -59,9 +59,32 @@ $(document).ready(function(){
     }
     
     if($("#dashboard").length){
-        printMilestones(dashboard.milestones[0].id);
-        printTasks(dashboard.milestones[0].tasks);
+        //printMilestones(dashboard.milestones[0].id);
+        printTasks($("#milestones li.selected").attr("milestone"));
+
+        //long polling new milestones, new tasks¿?
         
+        $("#tasks-filters select").val(0);
+        $("#tasks-filters").delegate("select", "change", function(){
+            var selects = $("#tasks-filters select");
+            var filters = new Array();
+            for(var i=0; i<selects.length; i++){
+                if($(selects[i]).val()!='0'){
+                    filters.push("["+$(selects[i]).attr('name')+"!="+$(selects[i]).val()+"]");
+                }
+            }
+            
+            //$("#tasks li").filter("[to!=Juanfran],[state!=Cerrada]").hide()
+            
+            if(filters.length>0){
+                $("#tasks").hide();
+                var tasks = $("#tasks li");
+                $(tasks).show();
+                $(tasks).filter(filters.join(',')).hide();
+                $("#tasks").show();
+            }else $("#tasks li").show();
+        }); 
+
         $("#tasks").delegate(".dg", "mousedown", function(e){
             var li = $(this).parent();
             $(li).css("position", "absolute");
@@ -84,46 +107,24 @@ $(document).ready(function(){
 
                 if(milestone.length && !$(milestone).hasClass('selected')){
                     //move the task    
-                    var id_milestone = parseInt($(milestone).attr('milestone'));
-                    var id_task = parseInt($('#inmove').attr('task'));
-                    var length_milestones  = dashboard.milestones.length;
-                    var length_tasks = 0;
-                    var task = 0;
-
-                    for(var i=0; i<length_milestones; i++){
-                        length_tasks = dashboard.milestones[i].tasks.length;
-                        for(var z=0; z<length_tasks; z++){
-                            if(dashboard.milestones[i].tasks[z].id==id_task){
-                                task = dashboard.milestones[i].tasks[z];
-                                dashboard.milestones[i].tasks.splice(z,1); 
-                                break;
-                            }
-                        }
-                        if(task)break;
-                    }
+                    var data = {};
+                    data.milestone = parseInt($(milestone).attr('milestone'));
+                    data.task = parseInt($('#inmove').attr('task'));
+                    $.ajax({url : '', type : 'POST', data: data});
                     
-                    if(!task){
-                        length_tasks = dashboard.tasks.length;
-                        for(z=0; z<length_tasks; z++){
-                            if(dashboard.tasks[z].id==id_task){
-                                task = dashboard.tasks[z];
-                                dashboard.tasks.splice(z,1); 
-                                break;
-                            }
-                        }                    
-                    }
-
-                    if(task){
-                        if(id_milestone){
-                            for(i=0; i<length_milestones; i++){
-                                if(dashboard.milestones[i].id==id_milestone){
-                                    dashboard.milestones[i].tasks.push(task);
-                                    break;
-                                }    
-                            }
-                        }else dashboard.tasks.push(task);
-                        printMilestones($("#milestones").find('.selected').attr('milestone'));
-                    }
+                    var tasks = $(milestone).find('span').html().split("/");
+                    if($('#inmove').attr('state')=='Cerrada')tasks[0] = parseInt(tasks[0])+1;
+                    
+                    tasks[1] = parseInt(tasks[1])+1;
+                    $(milestone).find('span').html(tasks[0]+"/"+tasks[1]);
+                    
+                    var old_milestone_span = $("#milestones li[milestone='"+$("#tasks").attr('milestone')+"'] span");
+                    tasks = $(old_milestone_span).html().split("/");
+                    
+                    if($('#inmove').attr('state')=='Cerrada')tasks[0] = parseInt(tasks[0])-1;
+                    
+                    tasks[1] = parseInt(tasks[1])-1;
+                    $(old_milestone_span).html(tasks[0]+"/"+tasks[1]);                    
 
                     $('#inmove').remove();
                 }else{
@@ -138,24 +139,13 @@ $(document).ready(function(){
             $("#milestones").find(".selected").removeClass("selected");
             var li = $(this).parent()
             $(li).addClass("selected");
-            var id = parseInt($(li).attr("milestone"));
-
-            if(id){
-                var length_milestones  = dashboard.milestones.length;
-                for(var i=0; i<length_milestones; i++){
-                    if(dashboard.milestones[i].id == id){
-                        printTasks(dashboard.milestones[i].tasks);
-                        break;
-                    }
-                }
-            }else printTasks(dashboard.tasks);
+            printTasks(parseInt($(li).attr("milestone")))
         });            
         
         $("#dashboard").delegate(".edit", "click", function(e){
             alert("popup");
             e.preventDefault();
-        })        
-        
+        })
     }
     
     if($(".messages-container li").length>0){
@@ -165,15 +155,27 @@ $(document).ready(function(){
     }
 })
 
-function printTasks(list){
-    length_tasks = list.length;
-    var html = '';        
-    for(var i=0; i<length_tasks; i++){
-        html+= '<li task="'+list[i].id+'" ><div class="dg">'+list[i].name+'<span>[Asignado a: '+list[i].assignedto+' | Estado: '+list[i].state+' | Prioridad: '+list[i].priority+' | Tipo: '+list[i].type+']</span></div><a class="edit" href=""><img width="21" src="imgs/cog.png" /></a></li>';
-    }
-    $("#tasks").html(html);
+function printTasks(milestone){
+    $("#tasks").attr('milestone', milestone);
+    var data = {};
+    data.milestone = milestone;
+    $.ajax({
+        url : 'auxiliar/json/dashboard.json',
+        type : 'GET',
+        data: data,
+        dataType: "json",
+        success: function(data){
+            length_tasks = data.tasks.length;
+            var html = '';        
+            for(var i=0; i<length_tasks; i++){
+                html+= '<li state="'+data.tasks[i].state+'"  to="'+data.tasks[i].assignedto+'" priority="'+data.tasks[i].priority+'" type="'+data.tasks[i].type+'" task="'+data.tasks[i].id+'" ><div class="dg">'+data.tasks[i].name+'<span>[Asignado a: '+data.tasks[i].assignedto+' | Estado: '+data.tasks[i].state+' | Prioridad: '+data.tasks[i].priority+' | Tipo: '+data.tasks[i].type+']</span></div><a class="edit" href=""><img width="21" src="imgs/cog.png" /></a></li>';
+            }
+            $("#tasks").html(html);
+        }
+    });
 }
 
+//borrar¿?
 function printMilestones(sel){
     var length_milestones  = dashboard.milestones.length;
     var length_tasks = 0;
