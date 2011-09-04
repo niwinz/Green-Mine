9a7cd2d9176d221d6128693d41d60fe402ba772e
Andrei Antoukh <niwi@niwi.be>
2011-09-04 17:23:11 +0200

    Fix

--- src/manage.py
+++ src/manage.py
@@ -4,7 +4,7 @@ import os, sys
 current_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(current_dir, 'extern'))
 sys.path.insert(0, os.path.join(current_dir, 'extern', 'django-randomfilenamestorage'))
-sys.path.insert(0, os.path.join(current_dir, 'extern', 'django-html5-forms')
+sys.path.insert(0, os.path.join(current_dir, 'extern', 'django-html5-forms'))
 
 from django.core.management import execute_manager
 import settings.local as settings
