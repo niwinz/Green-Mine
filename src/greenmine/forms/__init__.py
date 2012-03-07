4171280d44e2905322d13a2ae430597681ca47d2
Andrei Antoukh <niwi@niwi.be>
2012-03-07 21:58:18 +0100

    Syntax mistake.

--- src/greenmine/forms/__init__.py
+++ src/greenmine/forms/__init__.py
@@ -95,7 +95,7 @@ class LoginForm(Form):
 
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(max_length=200, min_length=4
+    username = forms.CharField(max_length=200, min_length=4,
         required=True, label=_(u'Username'))
     first_name = forms.CharField(max_length=200, 
         required=True, label=_(u"First name"))
