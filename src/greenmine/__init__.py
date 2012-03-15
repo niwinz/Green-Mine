2270034fe72771e5dc95c176abbe9bfc4209da49
Andrei Antoukh <niwi@niwi.be>
2012-03-15 23:06:59 +0100

    Deactivate old sqlite optimizations.

--- src/greenmine/__init__.py
+++ src/greenmine/__init__.py
@@ -3,10 +3,10 @@
 from django.db.backends.signals import connection_created
 from django.dispatch import receiver
 
-@receiver(connection_created)
-def connection_created_handler(sender, connection, **kwargs):
-    c = connection.cursor()
-    c.execute('PRAGMA temp_store = MEMORY;')
-    c.execute('PRAGMA synchronous = OFF;')
-    #c.execute('PRAGMA default_cache_size = 10000;')
-    print "Connection created: ", connection
+#@receiver(connection_created)
+#def connection_created_handler(sender, connection, **kwargs):
+#    c = connection.cursor()
+#    c.execute('PRAGMA temp_store = MEMORY;')
+#    c.execute('PRAGMA synchronous = OFF;')
+#    #c.execute('PRAGMA default_cache_size = 10000;')
+#    print "Connection created: ", connection
