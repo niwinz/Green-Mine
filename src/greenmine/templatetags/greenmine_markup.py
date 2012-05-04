dc5abd44042d224f939e8e65f9ef76c7e66e0514
Andrei Antoukh <niwi@niwi.be>
2012-05-05 00:27:15 +0200

    Finished experimental and uncompleted restructured text integration with code-block directive.

--- src/greenmine/templatetags/greenmine_markup.py
+++ src/greenmine/templatetags/greenmine_markup.py
@@ -12,7 +12,7 @@ import markdown
 
 from django.core.urlresolvers import reverse
 
-@register.filter(name="markdown")
+@register.filter(is_safe=True, name="markdown")
 def markdown_filter(value, project):
     res = markdown.markdown(
         value,
@@ -27,15 +27,17 @@ def markdown_filter(value, project):
     return res
 
 
-#from django.contrib.markup.templatetags.markup import restructuredtext
-#from docutils.parsers.rst import directives
-#from greenmine.utils.markup import CodeBlock
-#
-#directives.register_directive('code-block', CodeBlock)
-#
-#@register.filter(is_safe=True, name="rst")
-#def rst_filter(valie, project):
-#    pass
-#    
-#    
-#    
+from django.contrib.markup.templatetags.markup import restructuredtext
+from docutils.parsers.rst import directives
+from docutils.core import publish_parts
+from greenmine.utils.markup import CodeBlock
+
+directives.register_directive('code-block', CodeBlock)
+docutils_settings = getattr(settings, "RESTRUCTUREDTEXT_FILTER_SETTINGS", {}) 
+
+@register.filter(is_safe=True, name="rst")
+def rst_filter(value, project):
+    return restructuredtext(value)
+    parts = publish_parts(source=smart_str(value), writer_name="html4css1",
+        settings_overrides=docutils_settings)
+    return mark_safe(force_unicode(parts["fragment"]))
--- src/greenmine/utils/markup.py
+++ src/greenmine/utils/markup.py
@@ -4,28 +4,8 @@ import sys
 import codecs
 
 from docutils import nodes
-from docutils.parsers.rst import Directive
+from docutils.parsers.rst import Directive, directives
 
-def parselinenos(spec, total):
-    """Parse a line number spec (such as "1,2,4-6") and return a list of
-    wanted line numbers.
-    """
-    items = list()
-    parts = spec.split(',')
-    for part in parts:
-        try:
-            begend = part.strip().split('-')
-            if len(begend) > 2:
-                raise ValueError
-            if len(begend) == 1:
-                items.append(int(begend[0])-1)
-            else:
-                start = (begend[0] == '') and 0 or int(begend[0])-1
-                end = (begend[1] == '') and total or int(begend[1])
-                items.extend(xrange(start, end))
-        except Exception:
-            raise ValueError('invalid line number spec: %r' % spec)
-    return items
 
 def set_source_info(directive, node):
     node.source, node.line = \
@@ -42,29 +22,12 @@ class CodeBlock(Directive):
     required_arguments = 1
     optional_arguments = 0
     final_argument_whitespace = False
-    option_spec = {
-        'linenos': directives.flag,
-        'emphasize-lines': directives.unchanged_required,
-    }
 
     def run(self):
         code = u'\n'.join(self.content)
-
-        linespec = self.options.get('emphasize-lines')
-        if linespec:
-            try:
-                nlines = len(self.content)
-                hl_lines = [x+1 for x in parselinenos(linespec, nlines)]
-            except ValueError, err:
-                document = self.state.document
-                return [document.reporter.warning(str(err), line=self.lineno)]
-        else:
-            hl_lines = None
-
+        
         literal = nodes.literal_block(code, code)
-        literal['language'] = self.arguments[0]
-        literal['linenos'] = 'linenos' in self.options
-        if hl_lines is not None:
-            literal['highlight_args'] = {'hl_lines': hl_lines}
+        literal['classes'] = ['brush: java;']
+
         set_source_info(self, literal)
         return [literal]
