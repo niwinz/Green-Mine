063c6ce03f3657dc7c48cc209ceb71a5ca32d450
Andrei Antoukh <niwi@niwi.be>
2012-05-27 18:38:04 +0200

    Remove unused application.

--- src/django_smailer/settings.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.conf import settings
-
-SMAILER_EMAIL_BACKEND = getattr(settings, 'SMAILER_EMAIL_BACKEND', 
-    'django.core.mail.backends.locmem.EmailBackend')
-
-SMAILER_BIND_ADDRESS = getattr(settings, 'SMAILER_BIND_ADDRESS', 'ipc:///tmp/smailer.sock')
