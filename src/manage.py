370495b51c421683182bb9c2f6ff37514070ed98
Andrei Antoukh <niwi@niwi.be>
2012-05-29 21:10:36 +0200

    Remove unused files.

--- src/manage.py
+++ src/manage.py
@@ -4,7 +4,6 @@ import os, sys
 current_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, os.path.join(current_dir, 'extern'))
 sys.path.insert(0, os.path.join(current_dir, 'extern', 'django-randomfilenamestorage'))
-sys.path.insert(0, os.path.join(current_dir, 'extern', 'django-html5-forms'))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
