ca3c603f08513d92ae430592eadf9a1dd5bd29e0
Andrei Antoukh <niwi@niwi.be>
2012-06-09 20:10:18 +0200

    Add testing.py settings.

---
+++ src/settings/testing.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from .development import *
+
+GREENQUEUE_BACKEND = 'greenqueue.backends.sync.SyncService'
+GREENQUEUE_WORKER_MANAGER = 'greenqueue.worker.sync.SyncManager'
