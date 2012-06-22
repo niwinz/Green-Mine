ba04b2a9ea2b5cd7208b8e87f32192a70468df2c
Andrei Antoukh <niwi@niwi.be>
2012-06-22 23:52:42 +0200

    Clear spaces.

--- src/manage.py
+++ src/manage.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
 import os, sys
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, os.path.join(current_dir, 'extern'))
-sys.path.insert(0, os.path.join(current_dir, 'extern', 'django-randomfilenamestorage'))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
--- src/wsgi.py
+++ src/wsgi.py
@@ -19,12 +19,6 @@ import sys
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings.local'
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.insert(0, os.path.join(current_dir, 'extern'))
-sys.path.insert(0, os.path.join(current_dir, 'extern', 'django-randomfilenamestorage'))
-sys.path.insert(0, os.path.join(current_dir, 'extern', 'django-html5-forms'))
-
-
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
