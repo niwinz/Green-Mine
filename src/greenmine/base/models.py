7407ef01250d54367a9ad28d8c30900f7187d15e
Andrei Antoukh <niwi@niwi.be>
2012-07-09 23:52:03 +0200

    Add uuid field and centralized uuid generation.

--- src/greenmine/base/models.py
+++ src/greenmine/base/models.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
 
+from django.db.models import signals
+from django.dispatch import receiver
 from django.db import models
+
+import uuid
+
+# Centralized uuid attachment and ref generation
+
+@receiver(signals.pre_save)
+def attach_uuid_and_create_unique_ref(sender, instance, **kwargs):
+    fields = sender._meta.get_all_field_names()
+
+    # if sender class does not have uuid field.
+    if "uuid" not in fields:
+        return
+
+    if not instance.uuid:
+        instance.uuid = unicode(uuid.uuid1())
