8f7829e98295292bcf543a5721600b30c51dec93
David Barragan <david.barragan@kaleidos.net>
2012-07-09 12:24:48 +0200

    Added models file to the document app

---
+++ src/greenmine/documents/models.py
@@ -0,0 +1,41 @@
+# -* coding: utf-8 -*-
+
+from django.conf import settings
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
+from django.contrib.contenttypes import generic
+from django.contrib.auth.models import User
+
+from greenmine.core.fields import WikiField
+
+
+class TemporalFile(models.Model):
+    attached_file = models.FileField(upload_to="temporal_files",
+        max_length=1000, null=True, blank=True)
+
+    owner = models.ForeignKey('auth.User', related_name='tmpfiles')
+    created_date = models.DateTimeField(auto_now_add=True)
+
+class Document(models.Model):
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(unique=True, max_length=200, blank=True)
+    description = WikiField(blank=True)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now_add=True)
+
+    project = models.ForeignKey('Project', related_name='documents')
+    owner = models.ForeignKey('auth.User', related_name='documents')
+    attached_file = models.FileField(upload_to="documents",
+        max_length=1000, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify_uniquely(self.title, self.__class__)
+        super(Document, self).save(*args, **kwargs)
+
+    @models.permalink
+    def get_delete_url(self):
+        return ('documents-delete', (),
+            {'pslug': self.project.slug, 'docid': self.pk})
