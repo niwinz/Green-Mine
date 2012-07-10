9f8794a9a6c510a8d226de61eced69b8f2bd9e23
David Barragan <david.barragan@kaleidos.net>
2012-07-10 17:57:08 +0200

    Made a big refactor, oh yeah

--- src/greenmine/questions/search_indexes.py
+++ src/greenmine/questions/search_indexes.py
@@ -1,31 +1,13 @@
 # -* coding: utf-8 -*-
 from haystack import indexes
-from .models import Question, QuestionResponse
+from .models import Question
 
 
 class QuestionIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True)
-    owner = indexes.CharField(model_attr='owner')
-    project = indexes.CharField(model_attr='project')
-    milestone = indexes.CharField(model_attr='milestone', null=True)
-    created_date = indexes.DateTimeField(model_attr='created_date')
+    text = indexes.CharField(document=True, use_template=True, template_name='search/indexes/question_text.txt')
 
     def get_model(self):
         return Question
-
-    def index_queryset(self):
-        return self.get_model().objects.all()
-
-
-class QuestionResponseIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True)
-    owner = indexes.CharField(model_attr='owner')
-    project = indexes.CharField(model_attr='project')
-    question  = indexes.CharField(model_attr='question')
-    created_date = indexes.DateTimeField(model_attr='created_date')
-
-    def get_model(self):
-        return QuestionResponse
 
     def index_queryset(self):
         return self.get_model().objects.all()
