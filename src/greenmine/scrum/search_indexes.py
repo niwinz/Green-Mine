b172d280c7b46f2a881fef27e446acc69c98f4b3
David Barragan <david.barragan@kaleidos.net>
2012-07-12 13:43:31 +0200

    Removed unnecessary index

--- src/greenmine/scrum/search_indexes.py
+++ src/greenmine/scrum/search_indexes.py
@@ -3,26 +3,6 @@ from haystack import indexes
 from .models import Project, Milestone, UserStory, Task
 
 
-class ProjectIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True, template_name='search/indexes/project_text.txt')
-
-    def get_model(self):
-        return Project
-
-    def index_queryset(self):
-        return self.get_model().objects.all()
-
-
-class MilestoneIndex(indexes.RealTimeSearchIndex, indexes.Indexable):
-    text = indexes.CharField(document=True, use_template=True, template_name='search/indexes/milestone_text.txt')
-
-    def get_model(self):
-        return Milestone
-
-    def index_queryset(self):
-        return self.get_model().objects.all()
-
-
 class UserStoryIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True, template_name='search/indexes/userstory_text.txt')
 
